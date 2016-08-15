7d7e8ab484a8d611bfbb88b48bcce0121bf20586
Josh Whitt <jw34036@gmail.com>
2016-08-15 00:15:36 -0500

    Minor refactoring

--- magicbus/src/main/java/org/light32/projects/magicbus/MagicBus.java
+++ magicbus/src/main/java/org/light32/projects/magicbus/MagicBus.java
@@ -1,11 +1,5 @@
 package org.light32.projects.magicbus;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -16,12 +10,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
 
 @SpringBootApplication
 @RestController
@@ -45,57 +41,47 @@ public class MagicBus {
  		MBMessage message = new MBMessage();
  		message.setBody(null);
  		message.setRequestMethod("get");
- 		 		
  		String msgStr = JsonUtils.toJSON(message);
- 		
- 		// hit kafka
- 		if(partition != null) { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, partition, null, msgStr)); 			
- 	 		log.info( "GET -> " + topic + ":"+partition+" ["+msgStr.length()+"]");
 
- 		}
- 		else { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, msgStr));
- 	 		log.info( "GET -> " + topic + " ["+msgStr.length()+"]");
+        produceMsg(topic, msgStr);
 
- 		}
- 		
- 		log.debug("     : " + msgStr);
- 		MBStatus out = new MBStatus("OK");
- 		return JsonUtils.toJSON(out);
- 	}
+        log.debug("     : " + msgStr);
+        MBStatus out = new MBStatus("OK");
+        return JsonUtils.toJSON(out);
+    }
 
- 	@RequestMapping(method = RequestMethod.POST, value="/{topic}")
- 	public String post(@PathVariable String topic, @RequestBody Object request) {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{topic}")
+    public String post(@PathVariable String topic, @RequestBody Object request) {
+
  		MBMessage message = new MBMessage();
  		message.setBody(request);
  		message.setRequestMethod("post");
  		String msgStr = JsonUtils.toJSON(message);
- 		
- 		// hit kafka
- 		if(partition != null) { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, partition, null, msgStr)); 			
- 	 		log.info( "POST -> " + topic + ":"+partition+" ["+msgStr.length()+"]");
 
- 		}
- 		else { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, msgStr));
- 	 		log.info( "POST -> " + topic + " ["+msgStr.length()+"]");
+        produceMsg(topic, msgStr);
 
- 		}
- 		
  		log.debug("      : " + msgStr);
 
  		MBStatus out = new MBStatus("OK");
  		return JsonUtils.toJSON(out);
  	}
 
- 	
- 	//
- 	////////////////////////////////////////////////////////////////////////////
- 	//
- 	
- 	// TODO make embedded tomcat work for PUT and DELETE
+    private void produceMsg(String topic, String msgStr) {
+        if (partition != null) {
+            kProducer.send(new ProducerRecord<>(topic, partition, null, msgStr));
+            log.info("GET -> " + topic + ":" + partition + " [" + msgStr.length() + "]");
+        } else {
+            kProducer.send(new ProducerRecord<>(topic, msgStr));
+            log.info("GET -> " + topic + " [" + msgStr.length() + "]");
+        }
+    }
+
+    //
+    ////////////////////////////////////////////////////////////////////////////
+    //
+
+    // TODO make embedded tomcat work for PUT and DELETE
  	@RequestMapping(method = RequestMethod.DELETE, value = "/{topic}/{id}")
  	public @ResponseBody void delete(@PathVariable String topic, @PathVariable String id) {
  		MBMessage message = new MBMessage();
@@ -104,13 +90,12 @@ public class MagicBus {
  		message.setRequestMethod("delete");
  			
  		// hit kafka
- 		if(partition != null) { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, partition, null, JsonUtils.toJSON(message))); 			
- 		}
- 		else { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, JsonUtils.toJSON(message)));
- 		}
- 	}
+        if (partition != null) {
+            kProducer.send(new ProducerRecord<>(topic, partition, null, JsonUtils.toJSON(message)));
+        } else {
+            kProducer.send(new ProducerRecord<>(topic, JsonUtils.toJSON(message)));
+        }
+    }
 
  	@RequestMapping(method = RequestMethod.PUT, value = "/{topic}/{id}")
  	public @ResponseBody Object put(@PathVariable String topic, @PathVariable String id, @RequestBody Object request) {
@@ -120,14 +105,13 @@ public class MagicBus {
  		message.setRequestMethod("put");
  		
 	 	// hit kafka
- 		if(partition != null) { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, partition, null, JsonUtils.toJSON(message))); 			
- 		}
- 		else { 
- 	 		kProducer.send(new ProducerRecord<String, String>(topic, JsonUtils.toJSON(message)));
- 		}
- 		
- 		MBStatus out = new MBStatus("OK");
+        if (partition != null) {
+            kProducer.send(new ProducerRecord<>(topic, partition, null, JsonUtils.toJSON(message)));
+        } else {
+            kProducer.send(new ProducerRecord<>(topic, JsonUtils.toJSON(message)));
+        }
+
+        MBStatus out = new MBStatus("OK");
  		return JsonUtils.toJSON(out);
  	}
  	
@@ -143,8 +127,8 @@ public class MagicBus {
 		kafkaProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		kafkaProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-		kProducer = new KafkaProducer<String, String>(kafkaProperties);
-	}
+        kProducer = new KafkaProducer<>(kafkaProperties);
+    }
 
 	private static void initializeConfig(String[] args) {
 		if(args.length == 0 || args[0] == null) {
@@ -162,9 +146,6 @@ public class MagicBus {
 		try {
 			InputStream propfileStream = new FileInputStream(mbPropFileName);
 			mbProperties.load(propfileStream);
-		} catch (FileNotFoundException e) {
-			log.error(e.getMessage(),e);
-			throw new RuntimeException(e);
 		} catch (IOException e) {
 			log.error(e.getMessage(),e);
 			throw new RuntimeException(e);
